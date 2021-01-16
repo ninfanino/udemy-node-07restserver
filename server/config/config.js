efa394fec48fb030062a0b7464006665986ba40f
Ninfa Niño <ninfa_sys@hotmail.com>
2021-01-16 14:37:41 -0600

    delete comments

--- server/config/config.js
+++ server/config/config.js
@@ -16,13 +16,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlBD;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlBD = 'mongodb://localhost:27017/cafe'
-// } else {
-//     urlBD = 'mongodb://ninfan:MEreDIthGRey12_@cluster0-shard-00-00.brxyf.mongodb.net:27017,cluster0-shard-00-01.brxyf.mongodb.net:27017,cluster0-shard-00-02.brxyf.mongodb.net:27017/test?replicaSet=atlas-b8vxba-shard-0&ssl=true&authSource=admin'
-// }
-
-urlBD = 'mongodb://ninfan:MEreDIthGRey12_@cluster0-shard-00-00.brxyf.mongodb.net:27017,cluster0-shard-00-01.brxyf.mongodb.net:27017,cluster0-shard-00-02.brxyf.mongodb.net:27017/cafe?replicaSet=atlas-b8vxba-shard-0&ssl=true&authSource=admin'
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe'
+} else {
+    urlBD = 'mongodb://ninfan:MEreDIthGRey12_@cluster0-shard-00-00.brxyf.mongodb.net:27017,cluster0-shard-00-01.brxyf.mongodb.net:27017,cluster0-shard-00-02.brxyf.mongodb.net:27017/test?replicaSet=atlas-b8vxba-shard-0&ssl=true&authSource=admin'
+}
 
 
 process.env.URLDB = urlBD;
