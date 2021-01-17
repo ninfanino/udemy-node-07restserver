d07d6b0a6943d2c1faeaab582fb6bb660ad05c96
Ninfa Niño <ninfa_sys@hotmail.com>
2021-01-17 12:40:42 -0600

    update db name

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ console.log('env', process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe'
 } else {
-    urlBD = 'mongodb://ninfan:MEreDIthGRey12_@cluster0-shard-00-00.brxyf.mongodb.net:27017,cluster0-shard-00-01.brxyf.mongodb.net:27017,cluster0-shard-00-02.brxyf.mongodb.net:27017/test?replicaSet=atlas-b8vxba-shard-0&ssl=true&authSource=admin'
+    urlBD = 'mongodb://ninfan:MEreDIthGRey12_@cluster0-shard-00-00.brxyf.mongodb.net:27017,cluster0-shard-00-01.brxyf.mongodb.net:27017,cluster0-shard-00-02.brxyf.mongodb.net:27017/cafe?replicaSet=atlas-b8vxba-shard-0&ssl=true&authSource=admin'
 }
 
 
